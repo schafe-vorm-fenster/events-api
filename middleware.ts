8c10da541595294466e2008b3d89c0975ac29aef
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2023-08-14 09:52:24 +0200

    Fix middleware path matcher.

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ["/api/*"],
+  matcher: ["/api/:path*"],
 };
 
 export function middleware(req: NextRequest) {
